1899a025e3796e9fe412a3a2b406a454b3b5e59e
cganzon <crganzon@gmail.com>
2019-04-28 00:53:31 -0700

    displaying books on page

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -25,6 +25,13 @@ class Search extends Component {
             );
     }
 
+    saveBook = (book) => {
+        // alert("Book Saved!");
+        API.save(book)
+            .then(res => alert("Book Saved!"))
+            .catch(err => console.log(err));
+    }
+
     render() {
         return (
             <div>
@@ -45,15 +52,23 @@ class Search extends Component {
                                     src={book.volumeInfo.imageLinks
                                         ? book.volumeInfo.imageLinks.thumbnail
                                         : "https://via.placeholder.com/150"}
-                                        alt=""
+                                    alt=""
                                     className="card-img-top"
                                     style={{ height: "250px", width: "160px", padding: "10px" }}
                                 />
                                 <h5><strong>{book.volumeInfo.title}</strong></h5>
                                 <p><strong>Author(s)</strong>: {book.volumeInfo.authors ? book.volumeInfo.authors.join(", ") : "N/A"}</p>
                                 <p><strong>Description</strong>: {book.volumeInfo.description}</p>
-                                <a href={book.volumeInfo.infoLink} target="_blank" className="btn btn-primary mr-2">View</a>
-                                <a href="#" className="btn btn-danger">Save</a>
+                                <a href={book.volumeInfo.infoLink} target="_blank" rel="noopener noreferrer" className="btn btn-primary mr-2">View</a>
+                                <button onClick={() => this.saveBook({
+                                    title: book.volumeInfo.title,
+                                    authors: book.volumeInfo.authors ? book.volumeInfo.authors.join(", ") : "N/A",
+                                    description: book.volumeInfo.description,
+                                    image: book.volumeInfo.imageLinks
+                                        ? book.volumeInfo.imageLinks.thumbnail
+                                        : "https://via.placeholder.com/150",
+                                    link: book.volumeInfo.infoLink
+                                })} className="btn btn-danger">Save</button>
                             </li>
                         )
                     })}
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,5 +4,8 @@ export default {
     search: function (title) {
         // This will be a make a GET request with axios to /api/members
         return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${title}`);
+    },
+    save: function(book) {
+        return axios.post("/api/books", book);
     }
 };
